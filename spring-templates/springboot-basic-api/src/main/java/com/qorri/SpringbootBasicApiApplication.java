29d74e39f89276f870214ad713cf6c6fd5ebf58c
MrR007 <qorri24j@gmail.com>
2024-07-22 19:59:32 +0700

    Spring-basic-api

---
+++ spring-templates/springboot-basic-api/src/main/java/com/qorri/SpringbootBasicApiApplication.java
@@ -0,0 +1,13 @@
+package com.qorri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBasicApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBasicApiApplication.class, args);
+	}
+
+}
